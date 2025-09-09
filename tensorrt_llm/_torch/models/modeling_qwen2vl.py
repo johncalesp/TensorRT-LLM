10c2df5c383fac0997aab9a29b1e0e66e4184557
John Calderon <jcalderon@nvidia.com>
2025-09-09 19:47:57 +0000

    tmp stage
    Signed-off-by: John Calderon <jcalderon@nvidia.com>

--- tensorrt_llm/_torch/models/modeling_qwen2vl.py
+++ tensorrt_llm/_torch/models/modeling_qwen2vl.py
@@ -22,8 +22,8 @@ from ...sampling_params import SamplingParams
 from ..attention_backend import AttentionMetadata
 from ..model_config import ModelConfig
 from .modeling_auto import AutoModelForCausalLM
-from .modeling_multimodal_utils import (find_uncached_mm_embeds,
-                                        fuse_input_embeds)
+from .modeling_multimodal_utils import (find_input_mm_embeds, fuse_input_embeds,
+                                        get_multimodal_embeddings)
 from .modeling_utils import register_auto_model, register_vision_encoder
 
 DISAGG = os.getenv('TLLM_MULTIMODAL_DISAGGREGATED', '0') == '1'
@@ -616,8 +616,9 @@ class Qwen2VLModelBase(PreTrainedModel):
 
         if len(multimodal_params) > 0:
             if not DISAGG:
-                mm_embeds = self.mm_encoder.forward(
-                    multimodal_params[:num_context_requests])
+                mm_embeds = get_multimodal_embeddings(
+                    encoder_forward_fn=self.mm_encoder.forward,
+                    multimodal_params=multimodal_params[:num_context_requests])
             else:
                 mm_embeds = [
                     multimodal_param.multimodal_data["multimodal_embedding"]
@@ -626,7 +627,7 @@ class Qwen2VLModelBase(PreTrainedModel):
             mrope_config = self._parse_and_concat_mrope_config(
                 multimodal_params, num_context_requests,
                 num_generation_requests)
-            mm_embeds = find_uncached_mm_embeds(
+            mm_embeds = find_input_mm_embeds(
                 mm_embeds, multimodal_params[:num_context_requests])
 
         if 'mrope_position_deltas' in kwargs:
